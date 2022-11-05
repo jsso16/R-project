dcd6ed69c8c3ec6e09bcdf5ad6c63361d2a72884
jsso16 <wjsthwls24@daum.net>
2022-11-05 14:38:57 +0900

    Edit data_coll.R

--- data_coll.R
+++ data_coll.R
@@ -1,16 +1,16 @@
 # 1단계: 작업 폴더 설정
-install.packages("rstudioapi")
+# install.packages("rstudioapi")
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 getwd()
 
 # 2단계: 수집 대상 지역 설정
-loc <- read.csv("/Users/jeonsojin/R/R-project/sigun_code.csv")
+loc <- read.csv("./sigun_code.csv")
 loc$code <- as.character(loc$code)
 head(loc, 2)
 
 # 3단계: 수집 기간 설정
 datelist <- seq(from = as.Date('2021-01-01'),
-                to = as.Date('2021-04-30'),
+                to = as.Date('2021-12-31'),
                 by = '1 month')
 datelist <- format(datelist, format = '%Y%m')
 datelist[1:3]
