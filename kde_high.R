e750a26fd2c50e52af8d9b27860f939afbd5ea61
jsso16 <wjsthwls24@daum.net>
2022-11-05 14:45:27 +0900

    Add kde_high.R

---
+++ kde_high.R
@@ -0,0 +1,55 @@
+# 1단계: 실거래 + 그리드 데이터 결합
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+load("./06_geodataframe/06_apt_price.rdata")
+library(sf)
+grid <- st_read("./01_code/sigun_grid/seoul.shp")
+apt_price <- st_join(apt_price, grid, join = st_intersects)
+head(apt_price, 2)
+
+# 그리드별 평균 가격(평당) 계산
+kde_high <- aggregate(apt_price$py, by=list(apt_price$ID), mean)
+colnames(kde_high) <- c("ID", "avg_price")
+head(kde_high, 2)
+
+# 2단계: 그리드 + 평균 가격 결합
+kde_high <- merge(grid, kde_high, by="ID")
+# install.packages("ggplot2")
+library(ggplot2)
+library(dplyr)
+kde_high %>% ggplot(aes(fill = avg_price)) +
+                    geom_sf() +
+                    scale_fill_gradient(low = "white", high = "red")
+
+# 3단계: sp형으로 변환과 그리드별 중심 좌표 추출
+library(sp)
+kde_high_sp <- as(st_geometry(kde_high), "Spatial")
+x <- coordinates(kde_high_sp)[,1]
+y <- coordinates(kde_high_sp)[,2]
+
+# 기준 경계 설정
+l1 <- bbox(kde_high_sp)[1, 1] - (bbox(kde_high_sp)[1, 1] * 0.0001)
+l2 <- bbox(kde_high_sp)[1, 2] + (bbox(kde_high_sp)[1, 2] * 0.0001)
+l3 <- bbox(kde_high_sp)[2, 1] - (bbox(kde_high_sp)[2, 1] * 0.0001)
+l4 <- bbox(kde_high_sp)[2, 2] + (bbox(kde_high_sp)[2, 2] * 0.0001)
+
+# 지도 경계선 그리기
+# install.packages("spatstat")
+library(spatstat)
+win <- owin(xrange=c(l1, l2), yrange=c(l3, l4))
+plot(win)
+rm(list = c("kde_high_sp", "apt_price", "l1", "l2", "l3", "l4"))
+
+# 4단계: 밀도 그래프 표시하기
+p <- ppp(x, y, window = win)
+d <- density.ppp(p, weights = kde_high$avg_price,
+                 sigma = bw.diggle(p),
+                 kernel = "gaussian")
+plot(d)
+rm(list = c("x", "y", "win", "p"))
+
+# 5단계: 노이즈 제거와 래스터 이미지로 변환
+d[d < quantile(d)[4] + (quantile(d)[4] * 0.1)] <- NA
+# install.packages("raster")
+library(raster)
+raster_high <- raster(d)
+plot(raster_high)
